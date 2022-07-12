e3ded2007f04ef50af34add1a9459102ea6011bc
Irina Akhanteva <irinkayashina@gmail.com>
2022-07-11 14:21:06 -1100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,75 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  // let exprArr10 = [];
+
+  // for (let i = 0; i < expr.length; i = i + 10) {
+  //   exprArr10.push(expr.slice(i, i + 10));
+  // }
+
+  // let decodedArr = [];
+
+  // for(let item of exprArr10) {
+  //   let morseCode = [];
+
+  //   if (item === '**********') {
+  //     morseCode.push(' ');
+  //   }
+  //   else {
+  //     for (let i = 0; i < item.length; i = i + 2) {
+  //       let str2 = item.slice(i, i + 2);
+
+  //       if (str2 === '10') {
+  //         morseCode.push('.');
+  //       }
+  //       else if (str2 === '11') {
+  //         morseCode.push('-');
+  //       }
+  //     }
+  //   }
+  //   decodedArr.push(morseCode.join(''));
+  // }
+
+  // let result = '';
+  // for (let item of decodedArr) {
+  //   if (item === " ") {
+  //     result += ' ';
+  //   }
+  //   else {
+  //     result += MORSE_TABLE[item];
+  //   }
+  // }
+
+  // return result;
+
+let strMorse = ""
+let OutPhrase = ""
+let letter = ""
+
+let arr10 = expr.match(/.{1,10}/g)
+//(2) ['0000001011', '0010111110']
+for (let i of arr10) {
+  let arr2 = i.match(/.{1,2}/g)
+  //(5) ['00', '00', '00', '10', '11']
+  if (i === "**********"){
+    OutPhrase = OutPhrase + " ";
+    continue;
+  }
+  strMorse = "";
+  letter = "";
+  for (let k of arr2){
+    if (k === "10") {
+      strMorse = strMorse + ".";
+    } else if (k === "11") {
+      strMorse = strMorse + "-";
+    }
+  }
+  letter = MORSE_TABLE[strMorse];
+  OutPhrase = OutPhrase + letter;
+}
+
+return OutPhrase;
+
 }
 
 module.exports = {
